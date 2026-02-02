7a4f523eee19c8043eb4672e76afa21eb1470c30
nimeratus <nemcsics.benedek@gmail.com>
2026-02-02 09:13:30 +0100

    Update mouseWheel.js

--- packages/scratch-vm/src/io/mouseWheel.js
+++ packages/scratch-vm/src/io/mouseWheel.js
@@ -1,3 +1,5 @@
+const Timer = require('../util/timer');
+
 class MouseWheel {
     constructor (runtime) {
         /**
@@ -5,6 +7,36 @@ class MouseWheel {
          * @type{!Runtime}
          */
         this.runtime = runtime;
+
+        /**
+         * Amount of scrolling since the last hat block was triggered
+         * @type{number}
+         */
+        this.scrollBuildup = 0;
+
+        /**
+         * Measures time since last scroll event received
+         * @type{Timer}
+         */
+        this.timer = new Timer({
+            now: () => this.runtime.currentMSecs
+        });
+        this.timer.start();
+    }
+
+    /**
+     * How much scrolling triggers the hat block (pixels)
+     */
+    static get SCROLL_TRESHOLD () {
+        return 50;
+    }
+
+    /**
+     * After how much time without scroll events should
+     * the next event trigger a hat block instantly
+     */
+    static get SCROLL_TIMEOUT () {
+        return 1000;
     }
 
     /**
@@ -12,6 +44,11 @@ class MouseWheel {
      * @param  {object} data Data from DOM event.
      */
     postData (data) {
+        const timeSinceLastEvent = this.timer.timeElapsed();
+        this.timer.start();
+        
+        this.scrollBuildup += data.deltaY;
+        
         const matchFields = {};
         if (data.deltaY < 0) {
             matchFields.KEY_OPTION = 'up arrow';
@@ -21,7 +58,10 @@ class MouseWheel {
             return;
         }
 
-        this.runtime.startHats('event_whenkeypressed', matchFields);
+        if(Math.abs(this.scrollBuildup) > MouseWheel.SCROLL_TRESHOLD || timeSinceLastEvent > this.SCROLL_TIMEOUT) {
+            this.scrollBuildup = 0;
+            this.runtime.startHats('event_whenkeypressed', matchFields);
+        }
     }
 }
 
